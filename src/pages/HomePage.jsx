66dea0e0f51a246f9f39dbc8a2a7177d9b0584ec
yash <yashh2nd@gmail.com>
2025-05-01 15:15:47 +0530

    feat: create Home page structure

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductCategories from '../components/ProductCategories'
+
+function HomePage() {
+  return (
+    <div className='w-100 h-100'>
+
+        <ProductCategories/>
+      
+    </div>
+  )
+}
+
+export default HomePage
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import {createBrowserRouter} from "react-router"
 import App from "./App.jsx"
+import HomePage from "./pages/HomePage.jsx";
 
 const router = createBrowserRouter([
     {
@@ -7,7 +8,8 @@ const router = createBrowserRouter([
         element: <App/>,
         children:[
             {
-                
+                path: "/",
+                element: <HomePage/>
             }
         ]
     }
