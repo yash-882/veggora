0d1a4eae1470e280ed95cce453e7796bbd949199
yash <yashh2nd@gmail.com>
2025-05-01 22:23:00 +0530

    feat: create categories object for storing its details

---
+++ src/constants/categories.js
@@ -0,0 +1,45 @@
+import meat from "@assets/images/product-images/meat.jpg"
+import snacks from "@assets/images/product-images/snacks.webp"
+import vegetables from "@assets/images/product-images/vegetables.jpg"
+import dairy from "@assets/images//product-images/dairy.avif"
+import fruits from "@assets/images/product-images/fruits.avif"
+import bakery from "@assets/images/product-images/bakery.jpg"
+
+
+let categories = [
+    {
+        label: "Snacks",
+        image: snacks
+    },
+    {
+        label: "Dairy",
+        image: dairy
+    },
+    {
+        label: 'Vegetables',
+        image: vegetables
+    },
+    {
+        label: "Meat",
+        image: meat
+    },
+    {
+        label: "Fruits",
+        image: fruits
+    },
+    {
+        label: "Bakery",
+        image: bakery
+    }
+]
+    
+
+    // vegetables: "Vegetables",
+    // dairy: "Dairy",
+    // snacks: "Snacks",
+    // meat: "Meat",
+    // vegetables: "vegetables",
+    // bakery: "Bakery",
+    // fruits: "Fruits"
+
+export default categories;
