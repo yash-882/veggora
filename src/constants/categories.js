4e987e6e856e8877dbcf6a6e60fc8ec3ef6b8874
yash <yashh2nd@gmail.com>
2025-05-04 16:21:10 +0530

    style: add some new Product Categories

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,12 +1,19 @@
-import meat from "@assets/images/product-images/meat.jpg"
-import snacks from "@assets/images/product-images/snacks.webp"
-import vegetables from "@assets/images/product-images/vegetables.jpg"
-import dairy from "@assets/images//product-images/dairy.avif"
-import fruits from "@assets/images/product-images/fruits.avif"
-import bakery from "@assets/images/product-images/bakery.jpg"
+import meat from "@assets/images/product-category-images/meat.jpg"
+import snacks from "@assets/images/product-category-images/snacks.png"
+import vegetables from "@assets/images/product-category-images/vegetables.jpg"
+import dairy from "@assets/images//product-category-images/dairy.png"
+import fruits from "@assets/images/product-category-images/fruits.png"
+import bakery from "@assets/images/product-category-images/bakery.png"
+import healthAndWelless from "@assets/images/product-category-images/health_wellness.png"
+import babyFood from "@assets/images/product-category-images/baby_food.png"
+import skinCare from "@assets/images/product-category-images/skin_care.png"
 
 
 let categories = [
+    {
+        label: 'Vegetables',
+        image: vegetables
+    },
     {
         label: "Snacks",
         image: snacks
@@ -16,21 +23,30 @@ let categories = [
         image: dairy
     },
     {
-        label: 'Vegetables',
-        image: vegetables
+        label: "Fruits",
+        image: fruits
     },
     {
         label: "Meat",
         image: meat
     },
     {
-        label: "Fruits",
-        image: fruits
-    },
-    {
         label: "Bakery",
         image: bakery
-    }
+    },
+    {
+        label: "Health & Wellness",
+        image: healthAndWelless
+    },
+    {
+        label: "Baby Food",
+        image: babyFood
+    },
+    {
+        label: "Skin Care",
+        image: skinCare
+    },
+    
 ]
     
 
