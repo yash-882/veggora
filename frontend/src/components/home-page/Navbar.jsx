0fa0973c6c0866efdf8dca04a2f38bcb17ce3a6c
yash <yashh2nd@gmail.com>
2025-05-19 00:02:42 +0530

    refactor: cleanup navbar styles, remove unused css classes
    committ

--- frontend/src/components/home-page/Navbar.jsx
+++ frontend/src/components/home-page/Navbar.jsx
@@ -1,60 +1,121 @@
-import {Search, ShoppingCartIcon, UserCircle2, LucideShoppingBag, ListFilterIcon} from "lucide-react"
+import { Search, ShoppingCartIcon } from "lucide-react"
 import { useEffect } from "react";
-
+import { Link } from "react-router";
 
 function Navbar() {
 
     // This `useEffect` sets up a scroll event to make part of the navbar sticky at the top of the screen
-        useEffect(() => {
-            function stickNavbar() {
-                let sec1 = document.querySelector(".navbar-section-1"); 
-                let navContainer = document.querySelector(".custom-navbar");
-    
-                // When the user scrolls past the height of the navbar, make the first section sticky
-                if (window.scrollY >= navContainer.scrollHeight) 
-                    sec1.classList.add("stick-navbar");
+    useEffect(() => {
+        function stickNavbar(stickRemovalAnimation = true) {
+            //stick navContainer element for desktops
+            let navContainer = document.querySelector(".custom-navbar");
+            // for section-2 of the NavBar for small devices (contains search bar)
+            let sec2 = document.querySelector(".navbar-section-2");
 
-                 else 
-                    sec1.classList.remove("stick-navbar");
-                
+            // on medium/small devices
+            let isSmallDevice = window.innerWidth < 841;
+
+            //checks if the Navbar has crossed the viewport from the top
+            let shouldStick = window.scrollY > navContainer.offsetHeight
+            // main content (homepage content,etc)
+            let mainContent = document.querySelector('.main-content')
+
+            //(for medium and small screens) 
+            if (isSmallDevice) {
+                // always remove sticky behaviour from Nav-Container(desktop view)
+                navContainer.classList.remove('stick-navbar')
+
+                // stick navbar by checking 'shouldStick'*true -> stick, false->unstick*
+                sec2.classList.toggle("stick-navbar", shouldStick);
+
+                // setting margin to avoid the main content from crossing the top
+                mainContent.style.marginTop = shouldStick ?
+                    `${sec2.offsetHeight}px` : mainContent.style.marginTop = '0px'
             }
-    
-            // Attach scroll event listener
-            window.addEventListener('scroll', stickNavbar);
-    
-            // Clean up the listener when the component unmounts
-            return () => {
-                window.removeEventListener('scroll', stickNavbar);
+            // for desktops
+            else  {
+                // always remove sticky behaviour from section-1(mobile view) 
+                // before sticking the desktop's nav 
+                navContainer.classList.remove('stick-navbar')
+
+                // stick navbar by checking 'shouldStick'*true -> stick, false->unstick*
+                navContainer.classList.toggle("stick-navbar", shouldStick);
+
+                // setting margin to avoid the main content from crossing the top
+                mainContent.style.marginTop = shouldStick ?
+                    `${navContainer.offsetHeight}px` : mainContent.style.marginTop = '0px'
             }
-    
-        }, []);
+
+            // if navigation bar is sticked to the top
+            // then only remove animation should be applied
+            // both mobile and desktop use removal animation on navContainer
+            if (stickRemovalAnimation) {
+                let isSticked = sec2.classList.contains('stick-navbar') || 
+                navContainer.classList.contains('stick-navbar')
+
+                navContainer.classList.toggle("remove-animation-navbar", !isSticked);
+            }
+        }
+        // wait for a some milliseconds to improve performance
+        function debounce(func) {
+            let waitScrolling;
+            return function (...args) {
+                clearTimeout(waitScrolling)
+
+                waitScrolling = setTimeout(() => {
+                    func.call(this, ...args)
+                }, 100)
+            }
+        }
+
+        // calling stickyNavbar
+        const handleScroll = debounce(stickNavbar);
+
+        // on the first mount *passing false to prevent displaying stick removal animation*
+        handleScroll(false)
+        // Attach event listener for scrolling and resizing
+        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleScroll)
+
+        // Clean up the listener when the component unmounts
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleScroll);
+        }
+    }, []);
+
 
     return (
-        // Main navbar container: uses Bootstrap utility classes for layout and styling
-        <div className='container-fluid mx-0 px-1 py-2 custom-navbar d-flex justify-content-between align-items-center text-white'>
+        // Main navbar container: uses both css and bootstrap
+        <div className='container-fluid mx-0 px-1 py-1 custom-navbar d-flex justify-content-around align-items-center text-white'>
 
-            {/* SECTION 1 — Left part of the navbar with logo, filter button, and search bar */}
-            <div className="navbar-section-1 d-flex align-items-center">
+            {/* nav section-3 */}
+            <div className="navbar-section-1  d-flex align-items-center ">
+                <div className="location-info d-flex  align-items-center ">
 
-                     {/* Website logo (visible on desktop) */}
-                     <div className="veggora-logo-desktop">
-                    <a href="">
-                        <img src="/src/assets/images/website-logo.png" height="45px" alt="" />
-                    </a>
+                    <h5 role="link" className=" text-break  delivery-address-info mb-0 me-2   align-items-center">
+                        Add Location📍
+                        <a href="" className=" text-decoration-none px-1 py-1">Set</a>
+                    </h5>
                 </div>
 
-                {/* Filter button (with icon and label) */}
-                <div className='me-2 me-md-0 ms-md-0 ms-lg-2'>
+            </div>
+            {/* SECTION 1 — Left part of the navbar with logo,  and search bar */}
+            <div className="navbar-section-2 d-flex align-items-center">
+
+                {/* Website logo (visible on desktop) */}
+                <div className="me-2 veggora-logo-desktop">
                     <a
-                        role="button"
-                        className='nav-link filter-btn px-2 px-lg-2 py-1 d-flex h-100 align-items-center'>
-                        <ListFilterIcon color='#0d6efd' strokeWidth={3} />
-                        <p className='my-0 ms-1'>Filter</p>
+                        href="http://localhost:5173"
+                        className="d-block">
+                        <img src="/src/assets/images/website-logo.png"
+                            height={'48px'} alt="" />
                     </a>
                 </div>
-  
+
+
                 {/* Search bar */}
-                <div className='nav-input-group rounded-2 d-flex'>
+                <div className='nav-input-group  d-flex'>
                     <form action="" className='d-flex align-items-center w-100'>
                         {/* Search icon inside button */}
                         <button className='bg-transparent border border-0 px-3' >
@@ -64,62 +125,48 @@ function Navbar() {
                         {/* Search input */}
                         <input
                             type="text"
-                            className='text-white px-1 border rounded-start-0 rounded-end-2 border-0 h-100 w-100'
+                            className='text-white px-1 h-100 w-100'
                             placeholder='Search grocery items...' />
                     </form>
                 </div>
             </div>
 
             {/* SECTION 2 — Right part of the navbar with links like cart, orders, and user dropdown */}
-            <div className="navbar-section-2 py-lg-0 pb-1 pb-md-0 d-flex ">
+            <div className="navbar-section-3 py-lg-0 pb-1 pb-md-0 d-flex ">
 
-                 {/* Mobile logo (visible on small screens) */}
-                 <div className="veggora-logo-mobile me-4">
-                    <a href="">
+                {/* Mobile logo (visible on small screens) */}
+                <div className="veggora-logo-mobile me-4">
+                    <a href=""
+                        className="d-block">
                         <img src="/src/assets/images/website-logo.png" height="45px" alt="" />
                     </a>
                 </div>
 
-                   {/* Navigation icons and links */}
-                   <ul className='list-unstyled nav-options-ul gap-xl-5 gap-md-3 gap-3 d-flex my-0'>
+                {/* Navigation icons and links */}
+                <ul className='list-unstyled nav-options-ul gap-xl-4 gap-md-3 gap-3 d-flex my-0'>
 
-{/* Cart icon and label */}
-<li className='nav-item d-flex'>
-    <a href="" className='px-1 nav-link  d-flex border-0 rounded-3 align-items-center'>
-        <ShoppingCartIcon fill='white' size={26} />
-        <p className='para-link my-0 ms-1'>Cart</p>
-    </a>
-</li>
+                    {/* Cart icon and label */}
+                    <li className='nav-item d-flex'>
+                        <Link href="" className='px-2 nav-link  d-flex border-0 rounded-3 align-items-center'>
+                            <ShoppingCartIcon fill='white' size={26} />
+                            <p className='para-link my-0 ms-1'>Cart</p>
+                        </Link>
+                    </li>
 
-{/* Orders icon and label */}
-<li className='nav-item d-flex'>
-    <a href="" className='px-1 nav-link border-0 rounded-3 d-flex  align-items-center'>
-        <LucideShoppingBag fill='orange' color='rgb(31, 31, 31)' size={26} strokeWidth={2} />
-        <p className='para-link my-0 ms-1'>Orders</p>
-    </a>
-</li>
-
-{/* Sign-in dropdown with user icon */}
-<li className='nav-item d-flex align-items-center bg-success border border-0 dropdown rounded-5'>
-    <a
-        role="button"
-        href=""
-        id='navbar-login-opt'
-        className='px-2 py-md-2 h-100 py-lg-2 rounded-5 border-0 nav-link-login nav-link d-flex align-items-center'
-        data-bs-toggle="dropdown"
-        aria-expanded="false">
-        <UserCircle2 size={26} color='rgb(42, 42, 42)' strokeWidth={2} />
-        <p className='fw-bold my-0 ms-2'>Sign in</p>
-    </a>
-
-    {/* Dropdown options for signing in */}
-    <ul className='dropdown-menu dropdown-login-options dropdown-menu-dark' aria-labelledby='navbar-login-opt'>
-        <a href='' className="dropdown-item py-2 border border-0 rounded-2">Via Mobile number</a>
-        <a href='' className="dropdown-item py-2 border border-0 rounded-2">Via E-mail address</a>
-    </ul>
-</li>
-</ul>
+                    {/* Sign-in */}
+                    <li className='nav-item d-flex align-items-center nav-item-login rounded-4'>
+                        <Link
+                            href=""
+                            id='navbar-login-opt'
+                            className='px-3 py-md-2 h-100  py-lg-2 rounded-4 border-0 nav-link-login nav-link d-flex align-items-center'>
+                            <p className='fw-bold my-0 '>Sign in</p>
+                        </Link>
+                    </li>
+                </ul>
             </div>
+
+
+
         </div>
     )
 }
--- frontend/src/components/home-page/ProductCategories.jsx
+++ frontend/src/components/home-page/ProductCategories.jsx
@@ -1,47 +1,74 @@
 import React from 'react'
-import categories from "@constants/categories"
+import { Link } from 'react-router'
+import { dairy, snacks, personalCare, fruitsAndVegetables, babyCare } from "@constants/categories.js"
+import createUrlPath from '../../utils/functions/create-url-path'
+// categories of products
+const categories = [
+  [[...fruitsAndVegetables], 'Fruits & Vegetables'],
+  [[...dairy], 'Dairy'],
+  [[...snacks], 'Snacks'],
+  [[...babyCare], 'Baby Care'],
+ 
+]
 
 function ProductCategories() {
-
   return (
-    //contains categories including types and images
-    <>
-    <div
-      className='text-light category-container homepage-elements col-11 col-xl-11  text-center py-1 px-1 px-xl-1'>
-      <p className='py-1 fw-bolder '>
-       What are you looking for?
-        </p>
+    // contains categories including types and images
+    <div className="row gap-3 gap-xl-4 justify-content-evenly text-white">
+      {
+        // Each individual category(dairy, snacks, ets)
+        categories.map(([category, categoryName], index) => (
+          <div
+            className={
+              `col-12 col-md-10 row  category-list py-1 col-sm-10
+               ${category.length > 4 ? 'col-xl-12 order-first col-lg-12' : 'col-xl-4  col-lg-6'}
+               ${category.length < 4 ? 'order-last':''}`}
+            key={index}
+          >
+            {/* heading */}
+            <div className='d-flex mb-2 category-heading justify-content-between '>
+              {/* category name */}
+              <span
+                className='category-name'>
+                {categoryName}
+              </span>
 
-      {/* categories list */}
-      <ul className='list-unstyled gy-4 gy-xl-0   justify-content-evenly row'>
+              {/* Link to all products of a specific category */}
+              <span>
+                <Link
+                  to={`/products/${createUrlPath(categoryName)}`}
+                  className='text-decoration-none see-all-category-link  fw-light '>See all</Link>
+              </span>
+            </div>
 
-        {
-          // 
-         Object.keys(categories).map((category) => (
+            {
+              // each item category(chips, milk, etc)
+              category.map((prod, prodIndex) => (
+                // expands when there is more than 4 items in categories
+                <div className={`col-3 p-1 ${category.length > 4 ? 'col-xl-1 col-md-2 col-lg-2' : 'col-xl-3 col-md-2 col-lg-3'} `} key={prodIndex}>
 
-            // list categories 
-            <li
-              className='col-4  col-xl-1 col-lg-1 category-label'
-              key={category}>
-              <a
-                href=""
-                className='  d-flex flex-column align-items-center'>
-                {/*category image */}
-                <img
-                  src={categories[category].image}
-                  alt=""
-                />
+                  <div className="h-100 w-100 p-1 rounded-2 bg-dark">
 
-                {/* category name */}
-                <span>{categories[category].label}</span>
-              </a>
-            </li>
-          ))
-        }
-      </ul>
+                    {/* link */}
+                    <Link
+                      // modify paths (/snacks & sweets -> /snacks-sweets)
+                      to={`/products/${createUrlPath(categoryName)}/${createUrlPath(prod.productName)}`}
+                      className="h-100 w-100 category-link rounded-2 nav-link d-flex flex-column align-items-center justify-content-center">
+
+                      {/* product image */}
+                      <img src={prod.imageUrl} alt="" className="img-fluid" />
+                      {/* product label */}
+                      <p className="text-center text-break mt-1 mb-0">{prod.productName}</p>
+                    </Link>
+                  </div>
+                </div>
+              ))
+            }
+
+          </div>
+        ))
+      }
     </div>
-    
-    </>
   )
 }
 
